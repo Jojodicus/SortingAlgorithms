f19efa7d84108b72825f99f149520330eff8b43d
Jojodicus <76924386+Jojodicus@users.noreply.github.com>
2021-03-30 18:02:35 +0200

    Reversed insertion direction
    Makes code smaller and doesn't have to cache current element

--- Insertion.java
+++ Insertion.java
@@ -1,26 +1,18 @@
 public class Insertion {
-    @SuppressWarnings("ManualArrayCopy")
     public static void sort(int[] input) {
         // loop through each element
-        for (int i = 0; i < input.length; i++) {
-            int element = input[i];
-
-            // find its place in sorted par-list
-            int place = 0;
-            while (place < i) {
-                if (element < input[place]) {
-                    break;
-                }
-                place++;
+        for (int i = 1; i < input.length; i++) {
+            // swap it to the front until in right place
+            for (int j = i; j > 0 && input[j - 1] > input[j]; j--) {
+                swap(input, j, j - 1);
             }
-
-            // shift everything over to make room
-            for (int j = i; j > place; j--) {
-                input[j] = input[j - 1];
-            }
-            
-            // now it can be inserted
-            input[place] = element;
         }
+    }
+
+    // swaps index a and b
+    private static void swap(int[] input, int a, int b) {
+        int cache = input[a];
+        input[a] = input[b];
+        input[b] = cache;
     }
 }
