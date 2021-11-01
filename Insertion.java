18ca1acc787c03493d7d992b3e132ea746b130ed
Johannes <76924386+Jojodicus@users.noreply.github.com>
2021-11-01 14:17:58 +0100

    Update Insertion.java

--- Insertion.java
+++ Insertion.java
@@ -1,9 +1,9 @@
 public class Insertion {
     public static void sort(int[] input) {
         // loop through each element
-        for (int i = 1; i < input.length; i++) {
+        for (int i = 1; i < input.length; ++i) {
             // swap it to the front until in right place
-            for (int j = i; j > 0 && input[j - 1] > input[j]; j--) {
+            for (int j = i; j > 0 && input[j - 1] > input[j]; --j) {
                 swap(input, j, j - 1);
             }
         }
